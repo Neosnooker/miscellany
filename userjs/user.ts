fbfb36dcf987f9cd7e6a187632233113465de7eb
Neosnooker <h36ftbz0j@mozmail.com>
2022-12-17 22:12:23 +0700

    Update repository.

--- userjs/user.ts
+++ userjs/user.ts
@@ -35,7 +35,7 @@ user_pref("browser.onboarding.enabled", false);
 
 // Disable Pocket, a Firefox built-in extension.
 user_pref("extensions.pocket.enabled", false);
-
+user_pref("extensions.pocket.showHome", false);
 // Disable Firefox intrusive experiments and studies.
 user_pref("app.normandy.enabled", false);
 user_pref("messaging-system.rsexperimentloader.enabled", false);
@@ -51,6 +51,10 @@ user_pref("network.connectivity-service.enabled", false);
 user_pref("breakpad.reportURL", "");
 user_pref("browser.tabs.crashReporting.sendReport", false);
 
+// Do not use Firefox Merino.
+// Refer to https://mozilla-services.github.io/merino/ for more.
+user_pref("browser.urlbar.merino.enabled", false);
+
 /* Geolocation & Region */
 // Use Mozilla geolocation service if permission is granted.
 user_pref(
@@ -61,15 +65,22 @@ user_pref("geo.provider.use_geoclue", false);
 /// Disable OS geolocation when the user uses Windows.
 user_pref("geo.provider.ms-windows-location", false);
 
-// Do not update region
+// Do not update region.
 user_pref("browser.region.update.enabled", false);
 
-/* Safebrowsing */
-user_pref("browser.safebrowsing.*.enabled", false);
+/* Browser automatic update */
+// Do not update without permission.
+user_pref("app.update.auto", false);
+
+// Notify during download.
+user_pref("app.update.notifyDuringDownload", true);
 
 /* Other features */
 // Do not query search suggestions.
 user_pref("browser.search.suggest.enabled", false);
+
+// Disable push notifications.
+user_pref("dom.webnotifications.enabled", false);
 
 /**
  * [SECTION 2]: SECURITY
@@ -88,6 +99,22 @@ user_pref("network.dns.disableIPv6", true);
 // Prevent leakage of internal IP address when using peer connection (WebRTC).
 user_pref("media.peerconnection.ice.no_host", true);
 
+// Disable speculative connection.
+/// Link prefetch.
+user_pref("network.prefetch-next", false);
+/// DNS prefetch.
+user_pref("network.dns.disablePrefetch", true);
+/// Speculative pre-connections.
+user_pref("network.http.speculative-parallel-limit", 0);
+/// Network predictor.
+user_pref("network.predictor.enabled", false);
+/// Speculative connections from "browser.places" and URL bar.
+user_pref("browser.places.speculativeConnect.enabled", false);
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
+
+// Do not use service workers.
+user_pref("dom.serviceWorkers.enabled", false);
+
 /**
  * [SECTION 3]: Miscellaneous
  */
@@ -95,3 +122,16 @@ user_pref("media.peerconnection.ice.no_host", true);
 user_pref("browser.newtabpage.activity-stream.showSponsored", false);
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
 user_pref("browser.newtabpage.activity-stream.default.sites", "");
+
+// Do not show top stories.
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+
+// Disable Firefox View.
+user_pref("browser.tabs.firefox-view", false);
+
+// Disable several browser urlbar.suggest topics.
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.suggest.weather", false);
+
+// Enable sanitizer.
+user_pref("dom.security.sanitizer.enabled", true);
